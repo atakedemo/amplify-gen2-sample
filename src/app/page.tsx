635c276d0a0d65952d6fdfc8a0a27d60b971ddf3
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-08-10 16:29:43 +0900

    Amplifyのビルドエラー解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../../amplify_outputs.json";
 import { useEffect, useState } from "react";
 
 Amplify.configure(outputs);
