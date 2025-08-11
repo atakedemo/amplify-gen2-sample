42d9a3f40d30c93a19a600a3dca84803d2ff1aa4
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-08-11 14:41:34 +0900

    Amplifyのバックエンド修正（DB）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,94 @@
-// import { generateClient } from "aws-amplify/data";
-// import type { Schema } from "@/amplify/data/resource";
-// import { Amplify } from "aws-amplify";
-// import outputs from "@/amplify_outputs.json";
-// import { useEffect, useState } from "react";
+"use client";
 
-// Amplify.configure(outputs);
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import { useEffect, useState } from "react";
 
-// const client = generateClient<Schema>();
+Amplify.configure(outputs);
 
-// type TodoFormProps = {
-//   todo: Schema["Todo"]["type"];
-//   handleUpdate: (string: string) => void;
-//   handleDelete: () => void;
-// };
+const client = generateClient<Schema>();
 
-// const TodoForm = ({ todo, handleUpdate, handleDelete }: TodoFormProps) => {
-//   const [input, setInput] = useState<string>("");
+type TodoFormProps = {
+  todo: Schema["Todo"]["type"];
+  handleUpdate: (string: string) => void;
+  handleDelete: () => void;
+};
 
-//   useEffect(() => {
-//     setInput(todo.content || "");
-//   }, []);
+const TodoForm = ({ todo, handleUpdate, handleDelete }: TodoFormProps) => {
+  const [input, setInput] = useState<string>("");
 
-//   return (
-//     <div className="flex">
-//       <input
-//         type="text"
-//         value={input}
-//         onChange={(e) => setInput(e.target.value)}
-//       />
-//       <div className="flex gap-1">
-//         <button
-//           onClick={() => handleUpdate(input)}
-//           className="border-[1px] border-gray-500 px-2"
-//         >
-//           更新
-//         </button>
-//         <button
-//           onClick={handleDelete}
-//           className="border-[1px] border-gray-500 px-2"
-//         >
-//           削除
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    setInput(todo.content || "");
+  }, []);
+
+  return (
+    <div className="flex">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <div className="flex gap-1">
+        <button
+          onClick={() => handleUpdate(input)}
+          className="border-[1px] border-gray-500 px-2"
+        >
+          更新
+        </button>
+        <button
+          onClick={handleDelete}
+          className="border-[1px] border-gray-500 px-2"
+        >
+          削除
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
-  // const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
-  // const [input, setInput] = useState<string>("");
+  const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
+  const [input, setInput] = useState<string>("");
 
-  // const handleGetTodos = async () => {
-  //   const { data: todos } = await client.models.Todo.list();
-  //   setTodos(todos);
-  // };
+  const handleGetTodos = async () => {
+    const { data: todos } = await client.models.Todo.list();
+    setTodos(todos);
+  };
 
-  // const handleCreate = async () => {
-  //   await client.models.Todo.create({
-  //     content: input,
-  //   });
-  //   setInput("");
-  //   await handleGetTodos();
-  // };
+  const handleCreate = async () => {
+    await client.models.Todo.create({
+      content: input,
+    });
+    setInput("");
+    await handleGetTodos();
+  };
 
-  // const handleDelete = async (id: string) => {
-  //   await client.models.Todo.delete({
-  //     id,
-  //   });
-  //   handleGetTodos();
-  // };
+  const handleDelete = async (id: string) => {
+    await client.models.Todo.delete({
+      id,
+    });
+    handleGetTodos();
+  };
 
-  // const handleUpdate = async (id: string, content: string) => {
-  //   await client.models.Todo.update({
-  //     id,
-  //     content,
-  //   });
-  // };
+  const handleUpdate = async (id: string, content: string) => {
+    await client.models.Todo.update({
+      id,
+      content,
+    });
+  };
 
-  // useEffect(() => {
-  //   (async () => {
-  //     await handleGetTodos();
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      await handleGetTodos();
+    })();
+  }, []);
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      sample
-      {/* <div className="flex gap-1">
+      <div className="flex gap-1">
         <input
           type="text"
           value={input}
@@ -110,7 +111,7 @@ export default function Home() {
             />
           </div>
         );
-      })} */}
+      })}
       </main>
     </div>
   );
