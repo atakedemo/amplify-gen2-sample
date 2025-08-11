47842509d42b2a2b43b834f0eaf2cb081a0f5679
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-08-11 14:56:50 +0900

    Cognitoログインの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import outputs from "@/amplify_outputs.json";
 import { useEffect, useState } from "react";
 
@@ -87,44 +89,49 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="font-sans min-h-screen p-8 pb-20 sm:p-20">
-      <main className="max-w-2xl mx-auto flex flex-col gap-8">
-        <h1 className="text-2xl font-bold text-center mb-4">ToDo リスト</h1>
-        <div className="flex gap-2 justify-center">
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="新しいToDoを入力"
-            className="border border-gray-300 rounded px-3 py-2 w-full max-w-md"
-          />
-          <button
-            onClick={handleCreate}
-            className="border-[1px] border-gray-500 px-4 py-2 rounded hover:bg-gray-100 whitespace-nowrap"
-          >
-            追加
-          </button>
-        </div>
-        <div className="flex flex-col gap-4">
-          {todos.map((todo) => {
-            return (
-              <div key={todo.id} className="border border-gray-200 rounded p-4 bg-white shadow-sm">
-                <TodoForm
-                  todo={todo}
-                  handleUpdate={(content) => handleUpdate(todo.id, content)}
-                  handleDelete={() => handleDelete(todo.id)}
-                />
-              </div>
-            );
-          })}
-        </div>
-        {todos.length === 0 && (
-          <p className="text-center text-gray-500 mt-8">ToDo項目がありません。新しいToDoを追加してください。</p>
-        )}
-      </main>
-      <footer className="mt-16 text-center text-gray-500">
-        Footer
-      </footer>
-    </div>
+    <Authenticator>
+    {({ signOut, user }) => (
+      <div className="font-sans min-h-screen p-8 pb-20 sm:p-20">
+        <main className="max-w-2xl mx-auto flex flex-col gap-8">
+          <h1 className="text-2xl font-bold text-center mb-4">ToDo リスト</h1>
+          <h1>Hello {user?.username}</h1>
+          <div className="flex gap-2 justify-center">
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="新しいToDoを入力"
+              className="border border-gray-300 rounded px-3 py-2 w-full max-w-md"
+            />
+            <button
+              onClick={handleCreate}
+              className="border-[1px] border-gray-500 px-4 py-2 rounded hover:bg-gray-100 whitespace-nowrap"
+            >
+              追加
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            {todos.map((todo) => {
+              return (
+                <div key={todo.id} className="border border-gray-200 rounded p-4 bg-white shadow-sm">
+                  <TodoForm
+                    todo={todo}
+                    handleUpdate={(content) => handleUpdate(todo.id, content)}
+                    handleDelete={() => handleDelete(todo.id)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          {todos.length === 0 && (
+            <p className="text-center text-gray-500 mt-8">ToDo項目がありません。新しいToDoを追加してください。</p>
+          )}
+        </main>
+        <footer className="mt-16 text-center text-gray-500">
+            <button onClick={signOut}>Sign Out</button>
+        </footer>
+      </div>
+    )}
+    </Authenticator>
   );
 }
